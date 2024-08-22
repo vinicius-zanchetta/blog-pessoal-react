34816fe79cac8993dd585ab3b68d2d252888d9ce
vinicius zanchetta <viniciuszanchettadev@gmail.com>
2024-08-22 06:43:10 -0300

    Commit inicial

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,36 @@
+function Home() {
+    return (
+        <div style={{
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+        }}>
+            <div>
+                <div style={{
+                    width: "80vw",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}>
+                    <h2>Seja Bem Vinde!</h2>
+                    <p>Expresse aqui seus pensamentos e opiniões</p>
+                </div>
+                <div style={{
+                    width: "80vw",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}>
+                    <img
+                        src="https://i.imgur.com/VpwApCU.png"
+                        alt="Imagem de Página Home"
+                        width="400px" />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Home
